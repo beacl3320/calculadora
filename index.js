5d53c287a9a999a59511a0a093bcca16744b1f44
Beatriz Carpio$ git config --global user.name Beatriz <beatriz.carpio@bootcamp-upgrade.com>
2021-08-10 12:53:34 +0200

    FEAT: add funcionallity to calculator

---
+++ index.js
@@ -0,0 +1,19 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const buttonNumber = document.querySelectorAll('.number');
+const buttonOperator = document.querySelectorAll('.operator');
+
+//Probamos que funcione la calculadora en la consola, después lo podemos quitar del código
+/* const calculadora = new Calculadora();
+console.log(calculadora.sumar(2,3));
+console.log(calculadora.restar(2,3));
+console.log(calculadora.multiplicar(2,3));
+console.log(calculadora.dividir(2,3)); */
+
+const display = new Display(displayValorActual, displayValorAnterior);
+
+buttonNumber.forEach(button=> {
+    button.addEventListener('click', () => {
+        display.agregarNumero(button.innerHTML);
+    });
+});
